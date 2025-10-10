fa4472d3a07d19420f90556ba76023b18fce80ef
Simon Willover <simontalksalot@gmail.com>
2025-10-10 10:50:25 -0400

    okay, i think i figured out how to do it?

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,6 @@
 // including this bc VS Code gave me errors;
 import React from "react";
-import { Text, View, Pressable, Image } from "react-native";
-import "./index.css";
+import { Text, View, Pressable, Image, StyleProp, ViewStyle, TextStyle, StyleSheet } from "react-native";
 
 /*
       <Text
@@ -23,35 +22,76 @@ import "./index.css";
       </Text>
 */
 
+// it's a bummer I can't use CSS at all;
+
+/* way #1:
+const s_font: TextStyle = {
+    fontFamily: "'Times New Roman', Times, serif",
+    fontSize: 16,
+    color: "#000d20",
+}
+const s_navbar: StyleProp<ViewStyle> = {
+    display: "flex",
+    flex: 1,
+}
+const s_navbar_btn: StyleProp<ViewStyle> = {
+    width: 1,
+}
+const s_navbar_img: StyleProp<ViewStyle> = {
+    aspectRatio: "1 / 1",
+}
+const s_navbar_txt: TextStyle = {
+    textAlign: "center",
+    margin: "auto",
+}
+*/
+
+const style = StyleSheet.create({
+    font: {
+        fontFamily: "'Times New Roman', Times, serif",
+        fontSize: 16,
+        color: "#000d20",
+    },
+    navbar: {
+        display: "flex",
+        flex: 1,
+    },
+    navbar_btn: {
+        width: 1,
+    },
+    navbar_img: {
+        aspectRatio: "1 / 1",
+    },
+    navbar_txt: {
+        textAlign: "center",
+        margin: "auto",
+    }
+})
+
+
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-      >
-        <View className="navbar">
-          <Pressable>
+    <View>
+        <View style={style.navbar}>
+          <Pressable style={style.navbar_btn}>
             <Image src="home.png" />
-            <Text>Home</Text>
+            <Text style={[style.font, style.navbar_txt]}>Home</Text>
           </Pressable>
-          <Pressable>
+          <Pressable style={style.navbar_btn}>
             <Image src="search.png" />
-            <Text>Search</Text>
+            <Text style={[style.font, style.navbar_txt]}>Search</Text>
           </Pressable>
-          <Pressable>
+          <Pressable style={style.navbar_btn}>
             <Image src="post.png" />
-            <Text>Post</Text>
+            <Text style={[style.font, style.navbar_txt]}>Post</Text>
           </Pressable>
-          <Pressable>
+          <Pressable style={style.navbar_btn}>
             <Image src="messages.png" />
-            <Text>Messages</Text>
+            <Text style={[style.font, style.navbar_txt]}>Messages</Text>
           </Pressable>
-          <Pressable>
+          <Pressable style={style.navbar_btn}>
             <Image src="profile.png" />
-            <Text>Profile</Text>
+            <Text style={[style.font, style.navbar_txt]}>Profile</Text>
           </Pressable>
         </View>
     </View>
