b4ed652886013aa6b3497c05d7537a4c8c05f5ce
brynlamppa <bll9@calvin.edu>
2025-10-10 13:32:02 -0400

    Updated Prototype

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,23 @@
+<<<<<<< HEAD
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return <Stack />;
 }
+=======
+
+import { Stack } from "expo-router";
+
+export default function RootLayout() {
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <Stack.Screen name="index" />
+      <Stack.Screen name="(tabs)" />
+    </Stack>
+  );
+}
+>>>>>>> 53a5cc7 (Updated Prototype)
