0d94aad718d31ef56c91ae8873e2e09fe5f4d921
Simon Willover <simontalksalot@gmail.com>
2025-10-10 13:47:04 -0400

    enchantingment table test: add scrollable content to the middle;

--- app/index.tsx
+++ app/index.tsx
@@ -53,19 +53,28 @@ const style = StyleSheet.create({
         color: "#000d20",
     },
     app: {
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "stretch",
+        justifyContent: "space-around",
+        width: Dimensions.get("window").width,
         height: Dimensions.get("window").height,
+    },
+    content: {
+        overflowY: "scroll",
+        flexGrow: 1,
+        flexShrink: 1,
     },
     navbar: {
         display: "flex",
         flexDirection: "row",
-        flexGrow: 1,
+        flexGrow: 0,
         alignItems: "stretch",
         justifyContent: "space-around",
         width: Dimensions.get("window").width,
-        position: "absolute",
-        bottom: 0,
     },
     navbar_btn: {
+        flexGrow: 1,
         // width: 10,
         padding: 10,
         paddingBottom: 0,
@@ -84,10 +93,27 @@ const style = StyleSheet.create({
     }
 })
 
+const my_par = (<View>
+  <Text style={style.font}>
+    An item can be enchanted by using an enchanting table and placing the item and 1-3 lapis lazuli in the input slots. Upon placing the item, three (pseudo)randomized options appear on the right of the GUI. The glyphs, written in Standard Galactic Alphabet, do not affect the enchantment, but hovering over a presented enchantment shows one enchantment to be applied. On mobile devices, the player can tap an enchantment before putting in the lapis lazuli or hold the enchantment before release. The only choices available have a level requirement equal to or below the player's current level and a lapis lazuli requirement equal to or below the number of lapis lazuli placed in the table. Each option imbues the item with a randomized set of enchantments that are dependent on the number of experience levels required (e.g. a level 30 enchantment can give a pickaxe the "Efficiency II" enchantment); the actual level cost and the number of lapis lazuli required have no effect.
+  </Text>
+</View>);
 
 export default function Index() {
   return (
     <View style={style.app}>
+        <View style={style.content}>
+            {React.cloneElement(my_par)}
+            {React.cloneElement(my_par)}
+            {React.cloneElement(my_par)}
+            {React.cloneElement(my_par)}
+            {React.cloneElement(my_par)}
+            {React.cloneElement(my_par)}
+            {React.cloneElement(my_par)}
+            {React.cloneElement(my_par)}
+            {React.cloneElement(my_par)}
+            {React.cloneElement(my_par)}
+        </View>
         <View style={style.navbar}>
           <Pressable style={style.navbar_btn}>
             <Image style={style.navbar_img} source={require("../assets/images/icons/home.png")} />
